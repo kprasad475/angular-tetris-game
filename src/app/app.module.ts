384e1228f4d744edb58ec506a4fd061fec46b7e3
Binshabs <binshabs567@gmail.com>
2024-07-30 11:01:20 +0530

    tetris-game-implmentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,25 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { GameComponent } from './game/game.component';
+import { BoardComponent } from './board/board.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    BoardComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/controls/controls.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-controls',
+  templateUrl: './controls.component.html',
+  styleUrl: './controls.component.css'
+})
+export class ControlsComponent {
+
+}
